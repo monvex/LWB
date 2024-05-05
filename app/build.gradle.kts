8a02d7182ab96aa5118f67ee8db4e79c60cef78b
Mark Gerasimenko <mark.gerasimenko@yandex.ru>
2024-05-05 11:24:49 +0300

    Update LWBDatabase.kt

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,6 +56,10 @@ android {
         }
     }
 
+    tasks.withType<Test> {
+        useJUnitPlatform()
+    }
+
 }
 
 dependencies {
@@ -73,6 +77,12 @@ dependencies {
     implementation("androidx.compose.material3:material3-android:1.2.1")
     implementation("androidx.appcompat:appcompat:1.6.1")
     testImplementation("junit:junit:4.13.2")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.8.1")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
+    testImplementation("org.junit.jupiter:junit-jupiter-engine:5.8.1")
+    testImplementation("org.mockito:mockito-core:5.6.0")
+    testImplementation("org.mockito:mockito-junit-jupiter:4.2.0")
+    testImplementation("mysql:mysql-connector-java:8.0.28")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     androidTestImplementation(platform("androidx.compose:compose-bom:2024.05.00"))
@@ -82,6 +92,8 @@ dependencies {
     implementation("androidx.navigation:navigation-compose:2.7.7")
     implementation("androidx.lifecycle:lifecycle-runtime-compose:2.7.0")
     implementation("androidx.compose.material:material")
+
+    implementation("com.zaxxer:HikariCP:5.0.0")
 
     //ViewModel Compose
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0")
